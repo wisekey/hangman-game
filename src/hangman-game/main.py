946ebabfef32c296d04b0d0f35997bac67287a47
wisekey <bekov.tembulat.g@gmail.com>
2025-07-15 20:00:57 +0300

    feat: add function that prints welcome message to user

--- src/hangman-game/main.py
+++ src/hangman-game/main.py
@@ -4,3 +4,7 @@ path_to_file = 'src/hangman-game/ru_nouns.txt'
 def get_nouns_from_file(filename: str) -> list[str]:
     with open(filename, mode='r', encoding='utf-8') as file:
         return [noun.strip() for noun in file]
+    
+
+def print_welcome_message():
+    print('Добро пожаловать в игру "Висельник"')
