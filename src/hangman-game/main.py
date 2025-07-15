0fe7b263d9984ea33c5cd66d32c535e1a2fb9cef
wisekey <bekov.tembulat.g@gmail.com>
2025-07-15 19:59:30 +0300

    feat: add var that means path to the file

--- src/hangman-game/main.py
+++ src/hangman-game/main.py
@@ -1,3 +1,6 @@
+path_to_file = 'src/hangman-game/ru_nouns.txt'
+
+
 def get_nouns_from_file(filename: str) -> list[str]:
     with open(filename, mode='r', encoding='utf-8') as file:
         return [noun.strip() for noun in file]
